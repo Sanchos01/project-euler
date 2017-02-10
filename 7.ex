be2f9bf4fba5ed77e81e51ffe98dbec6e9ff65b9
Александр Туктаров <sanchos.1991@mail.ru>
2017-02-10 20:38:14 +0300

    improve 7

--- 7.ex
+++ 7.ex
@@ -1,16 +1,17 @@
 defmodule Elxr do
 
-  def findIndexedPrime(n), do: findIndexedPrime(n, 3, [2])
-  def findIndexedPrime(n, var, xs) do
-    cond do
-      n == length xs -> List.last(xs)
-      # n == length xs -> hd(xs)
-      Enum.reduce_while(xs, true, fn y, acc -> cond do
-        :math.pow(y, 2) > var -> {:halt, acc}
-        rem(var, y) == 0 -> {:halt, false}
-        true -> {:cont, acc}
-      end end) -> findIndexedPrime(n, var + 1, xs ++ [var])
-      true -> findIndexedPrime(n, var + 1, xs)
+  def findIndexedPrime(n), do: findIndexedPrime(n, 3, [2], [])
+  def findIndexedPrime(n, var, xs, cache) do
+    if Enum.reduce_while(xs, true, fn y, acc -> cond do
+      :math.pow(y, 2) > var -> {:halt, acc}
+      rem(var, y) == 0 -> {:halt, false}
+      true -> {:cont, acc}
+    end end) do cond do
+      :math.pow(List.last(xs), 2) < var -> findIndexedPrime(n, var, xs++Enum.reverse(cache), [])
+      n == 2 -> var
+      true -> findIndexedPrime(n - 1, var + 2, xs, [var|cache])
+    end else findIndexedPrime(n, var + 2, xs, cache)
     end
   end
+
 end
