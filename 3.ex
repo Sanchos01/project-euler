b6685bbf038ef67412f6403be9a2f6fa0471db44
александр туктаров <sanchos.1991@mail.ru>
2017-02-12 20:18:49 +0300

    light improve 3.ex

--- 3.ex
+++ 3.ex
@@ -4,19 +4,17 @@ defmodule Elxr do
     if rem(a, b) == 0, do: cutPrime(div(a, b), b), else: a
   end
 
-  def mainFunc(a, b, c) do
+  def mainFunc(a, 1), do: a - 1
+  def mainFunc(a, c) do
     cond do
-      rem(c, a) == 0 ->
-        mainFunc(a + 1, div(c, a), cutPrime(c, a))
-      a >= :math.sqrt(c) ->
-        Enum.reduce([a - 1, b, c], 0, fn x, acc -> if x > acc, do: x, else: acc end)
-      true ->
-        mainFunc(a + 1, b, c)
+      rem(c, a) == 0 -> mainFunc(a + 1, cutPrime(c, a))
+      :math.pow(a, 2) >= c -> c
+      true -> mainFunc(a + 1, c)
     end
   end
 
-  def testik do
-    mainFunc(2, 1, 600851475143)
+  def main do
+    mainFunc(2, 600851475143)
   end
 
 end
