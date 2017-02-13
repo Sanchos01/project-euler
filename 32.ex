e798b1a00bb7e1db5fab4dd51b455d6e8a3602d9
Александр Туктаров <sanchos.1991@mail.ru>
2017-02-13 20:28:24 +0300

    32 not yet

--- 32.ex
+++ 32.ex
@@ -1,18 +1,52 @@
 defmodule Elxr do
-import Enum, only: [reduce_while: 3, map: 2, uniq: 1]
+import Enum, only: [reduce_while: 3, uniq: 1]
 
-  def findDivisors(a) do
-    Enum.reduce(2..round(:math.sqrt(a)), [], fn x, acc -> if rem(a,x)==0,
-      do: (if :math.pow(x,2)==a, do: [[x,x]|acc], else: [[x,round(a/x)]|acc]), else: acc end)
+  def primesTo(a) do
+    Stream.filter(2..a, fn x -> isPrime(x) end)
   end
 
-  def checkPandigital(a) do
-    reduce_while(findDivisors(a), false, fn x, acc -> cond do
-      Enum.sort(numToDigits(a) ++ (map(x, fn y -> numToDigits(y) end) |> Enum.concat)) ==
-        [1,2,3,4,5,6,7,8,9] -> {:halt, true}
+  def isPrime(1), do: false
+  def isPrime(2), do: true
+  def isPrime(a) do
+    reduce_while(primesTo(a), true, fn x, acc -> cond do
+      :math.pow(x, 2) > a -> {:halt, acc}
+      rem(a, x) == 0 -> {:halt, false}
       true -> {:cont, acc}
     end end)
   end
+
+  def variations([], _), do: []
+  def variations(_, 0), do: []
+  def variations(xs, n) do
+    cond do
+      xs != uniq(xs) -> IO.puts("Elements not unique")
+      n > length(xs) -> IO.puts("n bigger than amount of elements of list")
+      true -> variations(xs, [], n)
+    end
+  end
+  def variations([], ys, _), do: [ys]
+  def variations(xs, ys, n) do
+    reduce_while(xs, [], fn x, acc -> if length(ys) == n, do: {:halt, acc ++ [ys]},
+      else: {:cont, acc ++ variations((xs -- [x]), (ys ++ [x]), n)} end)
+  end
+
+  def variationsToL(xs, ys, n) do
+    reduce_while(xs, [], fn x, acc -> if length(ys) == n, do: {:halt, acc ++ [ys]},
+      else: {:cont, acc ++ variations((xs -- [x]), ([x] ++ ys), n)} end)
+  end
+
+  def combs([], _), do: []
+  def combs(xs, 1), do: xs
+  def combs(xs, n), do: combs(xs, [], n)
+  def combs(xs, ys, n) do
+    reduce_while(xs, [], fn x, acc -> if length(ys) == n, do: {:halt, acc ++ [ys]},
+      else: {:cont, acc ++ combs(xs, ys ++ [x], n)} end)
+  end
+
+  def combsR(a), do: variations([1,2,3,4,5,6,7,8,9], numToDigits(a), countNums(a)+1)
+    |> Enum.map(fn x -> digitsToNum(x) end)
+  def combsL(a), do: variationsToL([1,2,3,4,5,6,7,8,9], numToDigits(a), countNums(a)+1)
+    |> Enum.map(fn x -> digitsToNum(x) end)
 
   def countNums(a) do
     if div(a, 10) > 0, do: 1 + countNums(div(a, 10)), else: 1
@@ -28,25 +62,29 @@ import Enum, only: [reduce_while: 3, map: 2, uniq: 1]
   defp digitsToNum(xs, 1), do: hd(xs)
   defp digitsToNum([x|xs], n), do: round(x * :math.pow(10, n-1)) + digitsToNum(xs, n-1)
 
-  def variations([], _), do: []
-  def variations(_, 0), do: []
-  def variations(xs, n) do
-    cond do
-      xs != uniq(xs) -> IO.puts("Elements not unique")
-      n > length(xs) -> IO.puts("n bigger than amount of elements of list")
-      true -> variations(xs, [], n)
-    end
+  def isTruncPrime(a) when is_integer(a), do: if a<10, do: false,
+    else: isTruncPrime(numToDigits(a))
+  def isTruncPrime(xs), do: isTruncPrimeL(xs) && isTruncPrimeR(xs)
+  def isTruncPrimeR(xs) when length(xs) == 1, do: isPrime(hd(xs))
+  def isTruncPrimeR([_|xs]), do: isPrime(digitsToNum(xs)) && isTruncPrimeR(xs)
+  def isTruncPrimeL(xs) when length(xs) == 1, do: isPrime(hd(xs))
+  def isTruncPrimeL(xs) do
+    ys = List.delete_at(xs, length(xs) - 1)
+    isPrime(digitsToNum(ys)) && isTruncPrimeL(ys)
   end
-  defp variations([], ys, _), do: [ys]
-  defp variations(xs, ys, n) do
-    reduce_while(xs, [], fn x, acc -> if length(ys) == n, do: {:halt, acc ++ [ys]},
-      else: {:cont, acc ++ variations((xs -- [x]), (ys ++ [x]), n)} end)
+
+  def biggerTrunc(a) do
+    (combsL(a) |> Enum.filter(fn x -> isTruncPrime(x) end)) ++
+    (combsR(a) |> Enum.filter(fn x -> isTruncPrime(x) end))
   end
 
   def main do
-    Elxr.variations([1,2,3,4,5,6,7,8,9], 4)
-    |> Enum.map(fn x -> Elxr.digitsToNum(x) end)
-    |> Enum.filter(fn x -> Elxr.checkPandigital(x) end) |> Enum.sum
+    combs([1,2,3,5,7,9], 5)
+    |> Enum.reject(fn xs -> hd(xs) == 1 && hd(xs) == 9 end)
+    |> Enum.map(fn x -> combs([3,7], x, 6) end)
+    # |> Enum.filter(fn xs -> hd(xs) != 1 && List.last(xs) != 1 end)
+    |> Enum.reduce([], fn x, acc -> acc ++ Enum.map(x, fn y -> digitsToNum(y) end) end)
+    # |> Enum.filter(fn x -> isPrime(x) && isTruncPrime(x) end)
   end
 
 end
