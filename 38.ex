f87ae68eba5ad644499c6239d6bf3f5294377860
александр туктаров <sanchos.1991@mail.ru>
2017-02-16 22:32:36 +0300

    solv 38

--- 38.ex
+++ 38.ex
@@ -15,16 +15,16 @@ defmodule Elxr do
   defp digitsToNum([x|xs], n), do: round(x * :math.pow(10, n-1)) + digitsToNum(xs, n-1)
 
   def multis(a, 1), do: numToDigits(a)
-  def multis(a, n) when a > 1, do: [multis(a, n - 1)|numToDigits(a * n)]
-
-  def collectPanMultis, do: collectPanMultis(2, 1, {0, 0})
+  def multis(a, n) when a > 1, do: multis(a, n - 1) ++ numToDigits(a * n)
+  
+  def collectPanMultis(a \\ 2, n \\ 1, tuple \\ {0, 0})
   def collectPanMultis(10000, _n, tuple), do: tuple
   def collectPanMultis(a, n, {num, multi}) do
     numCheck = multis(a, n)
     cond do
       length(numCheck) < 9 -> collectPanMultis(a, n + 1, {num, multi})
       length(numCheck) > 9 -> collectPanMultis(a + 1, 1, {num, multi})
-      numCheck -- [1,2,3,4,5,6,7,8,9] == 0 -> if digitsToNum(numCheck) > multi,
+      numCheck -- [1,2,3,4,5,6,7,8,9] == [] -> if digitsToNum(numCheck) > multi,
         do: collectPanMultis(a + 1, 1, {a, digitsToNum(numCheck)}),
         else: collectPanMultis(a + 1, 1, {num, multi})
       true -> collectPanMultis(a + 1, 1, {num, multi})
